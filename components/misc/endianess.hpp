e92bd5ed1b1539e0fca4bfe5c8b4326300c5a558
Douglas Mencken <dougmencken@gmail.com>
2013-02-17 05:14:37 -0500

    Update endianess.hpp for new byte-swapping solution. Rename FOURCHAR macro to OPENMW_FOURCHAR.

--- components/misc/endianess.hpp
+++ components/misc/endianess.hpp
@@ -5,7 +5,7 @@
 #include <endian.h>
 #include <OgrePlatform.h>
 
-#define FOURCHAR(a,b,c,d) ((uint32_t)(((a)<<24) | ((b)<<16) | ((c)<<8) | (d)))
+#define OPENMW_FOURCHAR(a,b,c,d) ((uint32_t)(((a)<<24) | ((b)<<16) | ((c)<<8) | (d)))
 
 float reverseFloat(const float inFloat);
 
@@ -20,5 +20,68 @@ float reverseFloat(const float inFloat);
 #  define betoh_float(f) reverseFloat(f)
 #  define htobe_float(f) reverseFloat(f)
 #endif
+
+template<class T>
+inline void endianSwap(T& t)
+{
+    union
+    {
+        T t;
+        unsigned char u8[sizeof(T)];
+    } in, out;
+
+    switch (sizeof(T))
+    {
+        case 4:
+            in.t = t;
+            out.u8[0] = in.u8[3];
+            out.u8[1] = in.u8[2];
+            out.u8[2] = in.u8[1];
+            out.u8[3] = in.u8[0];
+            t = out.t;
+            break;
+        case 2:
+            in.t = t;
+            out.u8[0] = in.u8[1];
+            out.u8[1] = in.u8[0];
+            t = out.t;
+            break;
+        case 1:
+            // nothing to be swapped
+            break;
+        default:
+            in.t = t;
+            for (size_t k = 0; k < sizeof(T); k++)
+                out.u8[k] = in.u8[sizeof(T)-1 - k];
+            t = out.t;
+            break;
+    }
+}
+
+struct SwapLEStruct
+{
+    template<typename T>
+    void operator()(T& t) const
+    {
+#if OGRE_ENDIAN == OGRE_ENDIAN_BIG
+        endianSwap<T>(t);
+#elif OGRE_ENDIAN == OGRE_ENDIAN_LITTLE
+        // do nothing
+#endif
+    }
+};
+
+struct SwapBEStruct
+{
+    template<typename T>
+    void operator()(T& t) const
+    {
+#if OGRE_ENDIAN == OGRE_ENDIAN_BIG
+        // do nothing
+#elif OGRE_ENDIAN == OGRE_ENDIAN_LITTLE
+        endianSwap<T>(t);
+#endif
+    }
+};
 
 #endif
